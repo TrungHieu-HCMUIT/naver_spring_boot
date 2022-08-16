773dc5407e2dccf4ee163e8c77d6a5ab60f33e86
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2022-08-16 14:10:38 +0700

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# naver_spring_boot
---
+++ src/main/java/com/example/Naver/spring/boot/NaverSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Naver.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverSpringBootApplication.class, args);
+	}
+
+}
