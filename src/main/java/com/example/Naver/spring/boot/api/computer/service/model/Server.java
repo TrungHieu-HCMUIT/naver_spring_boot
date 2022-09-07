e2b418b8a1c7dd2f917a0de39412c9ffff4f23bd
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2022-09-07 11:46:35 +0700

    Init

--- src/main/java/com/example/Naver/spring/boot/api/computer/service/model/Server.java
+++
@@ -1,8 +0,0 @@
-package com.example.naver.spring.boot.api.computer.service.model;
-
-public class Server implements Computer {
-    @Override
-    public String getType() {
-        return "Server";
-    }
-}
